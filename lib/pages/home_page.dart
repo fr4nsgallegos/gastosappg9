34728ff18545cf7708fb499c5cc2d322ab2ea18c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-25 12:00:51 -0500

    fondo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,55 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                color: Colors.black,
+                height: 100,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Agregar",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 75,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
